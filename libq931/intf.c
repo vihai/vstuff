d086d3b553cb104a229ad5858a6bea8cb902a51e
Matteo Brancaleoni <mbrancaleoni@espia.it>
2005-10-17 09:50:57 +0000

    Changed libq931's license back to GPL

--- libq931/intf.c
+++ libq931/intf.c
@@ -6,7 +6,7 @@
  * Authors: Daniele "Vihai" Orlandi <daniele@orlandi.com>
  *
  * This program is free software and may be modified and distributed
- * under the terms and conditions of the GNU Lesser General Public License.
+ * under the terms and conditions of the GNU General Public License.
  *
  */
 
