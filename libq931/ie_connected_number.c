88d2b4b1c46b0a22596d06988549321fd5cf637c
Daniele Orlandi <daniele@orlandi.com>
2006-01-04 00:10:48 +0000

    Addedd Connected Number IE (part 2)

---
+++ libq931/ie_connected_number.c
@@ -0,0 +1,243 @@
+/*
+ * vISDN DSSS-1/q.931 signalling library
+ *
+ * Copyright (C) 2004-2005 Daniele Orlandi
+ *
+ * Authors: Daniele "Vihai" Orlandi <daniele@orlandi.com>
+ *
+ * This program is free software and may be modified and distributed
+ * under the terms and conditions of the GNU General Public License.
+ *
+ */
+
+#include <string.h>
+#include <assert.h>
+
+#define Q931_PRIVATE
+
+#include <libq931/lib.h>
+#include <libq931/ie_connected_number.h>
+
+static const struct q931_ie_type *ie_type;
+
+void q931_ie_connected_number_register(
+	const struct q931_ie_type *type)
+{
+	ie_type = type;
+}
+
+struct q931_ie_connected_number *q931_ie_connected_number_alloc()
+{
+	struct q931_ie_connected_number *ie;
+	ie = malloc(sizeof(*ie));
+	assert(ie);
+
+	memset(ie, 0, sizeof(*ie));
+
+	ie->ie.refcnt = 1;
+	ie->ie.type = ie_type;
+
+	memset(ie->number, 0, sizeof(*ie->number));
+
+	return ie;
+}
+
+struct q931_ie *q931_ie_connected_number_alloc_abstract()
+{
+	return &q931_ie_connected_number_alloc()->ie;
+}
+
+int q931_ie_connected_number_read_from_buf(
+	struct q931_ie *abstract_ie,
+	const struct q931_message *msg,
+	int pos,
+	int len)
+{
+	assert(abstract_ie->type == ie_type);
+
+	struct q931_ie_connected_number *ie =
+		container_of(abstract_ie,
+			struct q931_ie_connected_number, ie);
+
+	int nextoct = 0;
+
+	if (len < 1) {
+		report_msg(msg, LOG_ERR, "IE size < 1\n");
+		return FALSE;
+	}
+
+	struct q931_ie_connected_number_onwire_3 *oct_3 =
+		(struct q931_ie_connected_number_onwire_3 *)
+		(msg->rawies + pos + (nextoct++));
+
+	ie->type_of_number = oct_3->type_of_number;
+	ie->numbering_plan_identificator = oct_3->numbering_plan_identificator;
+
+	if (!oct_3->ext) {
+		struct q931_ie_connected_number_onwire_3a *oct_3a =
+			(struct q931_ie_connected_number_onwire_3a *)
+			(msg->rawies + pos + (nextoct++));
+
+		ie->presentation_indicator = oct_3a->presentation_indicator;
+		ie->screening_indicator = oct_3a->screening_indicator;
+	} else {
+		ie->presentation_indicator =
+			Q931_IE_CN_PI_PRESENTATION_ALLOWED;
+		ie->screening_indicator =
+			Q931_IE_CN_SI_USER_PROVIDED_NOT_SCREENED;
+	}
+
+	memcpy(ie->number, msg->rawies + pos + nextoct, len - nextoct);
+	ie->number[len - nextoct] = '\0';
+
+	return TRUE;
+}
+
+int q931_ie_connected_number_write_to_buf(
+	const struct q931_ie *generic_ie,
+	void *buf,
+	int max_size)
+{
+	struct q931_ie_connected_number *ie =
+		container_of(generic_ie, struct q931_ie_connected_number, ie);
+	struct q931_ie_onwire *ieow = (struct q931_ie_onwire *)buf;
+
+	// Check max_size
+
+	ieow->id = Q931_IE_CALLING_PARTY_NUMBER;
+	ieow->len = 0;
+
+	ieow->data[ieow->len] = 0x00;
+	struct q931_ie_connected_number_onwire_3 *oct_3 =
+	  (struct q931_ie_connected_number_onwire_3 *)(&ieow->data[ieow->len]);
+	oct_3->ext = 0;
+	oct_3->type_of_number = ie->type_of_number;
+	oct_3->numbering_plan_identificator = ie->numbering_plan_identificator;
+	ieow->len++;
+
+	struct q931_ie_connected_number_onwire_3a *oct_3a =
+	  (struct q931_ie_connected_number_onwire_3a *)(&ieow->data[ieow->len]);
+	oct_3a->ext = 1;
+	oct_3a->presentation_indicator = ie->presentation_indicator;
+	oct_3a->screening_indicator = ie->screening_indicator;
+	ieow->len++;
+
+	memcpy(&ieow->data[ieow->len], ie->number, strlen(ie->number));
+	ieow->len += strlen(ie->number);
+
+	return ieow->len + sizeof(struct q931_ie_onwire);
+}
+
+static const char *q931_ie_connected_number_type_of_number_to_text(
+        enum q931_ie_connected_number_type_of_number
+                type_of_number)
+{
+        switch(type_of_number) {
+	case Q931_IE_CN_TON_UNKNOWN:
+		return "Unknown";
+	case Q931_IE_CN_TON_INTERNATIONAL:
+		return "International";
+	case Q931_IE_CN_TON_NATIONAL:
+		return "National";
+	case Q931_IE_CN_TON_NETWORK_SPECIFIC:
+		return "Network specific";
+	case Q931_IE_CN_TON_SUBSCRIBER:
+		return "Subscriber";
+	case Q931_IE_CN_TON_ABBREVIATED:
+		return "Abbreviated";
+	case Q931_IE_CN_TON_RESERVED_FOR_EXT:
+		return "Reserved";
+	default:
+		return "*INVALID*";
+	}
+}
+
+static const char *q931_ie_connected_number_numbering_plan_identificator_to_text(
+        enum q931_ie_connected_number_numbering_plan_identificator
+                numbering_plan_identificator)
+{
+        switch(numbering_plan_identificator) {
+	case Q931_IE_CN_NPI_UNKNOWN:
+		return "Unknown";
+	case Q931_IE_CN_NPI_ISDN_TELEPHONY:
+		return "ISDN Telephony";
+	case Q931_IE_CN_NPI_DATA:
+		return "Data";
+	case Q931_IE_CN_NPI_TELEX:
+		return "Telex";
+	case Q931_IE_CN_NPI_NATIONAL_STANDARD:
+		return "National standard";
+	case Q931_IE_CN_NPI_PRIVATE:
+		return "Private";
+	case Q931_IE_CN_NPI_RESERVED_FOR_EXT:
+		return "Reserved";
+	default:
+		return "*INVALID*";
+	}
+}
+
+static const char *q931_ie_connected_number_presentation_indicator_to_text(
+        enum q931_ie_connected_number_presentation_indicator
+                presentation_indicator)
+{
+        switch(presentation_indicator) {
+	case Q931_IE_CN_PI_PRESENTATION_ALLOWED:
+		return "Presentation allowed";
+	case Q931_IE_CN_PI_PRESENTATION_RESTRICTED:
+		return "Presentation restricted";
+	case Q931_IE_CN_PI_NOT_AVAILABLE:
+		return "Not available";
+	default:
+		return "*INVALID*";
+	}
+}
+
+static const char *q931_ie_connected_number_screening_indicator_to_text(
+        enum q931_ie_connected_number_screening_indicator
+                screening_indicator)
+{
+        switch(screening_indicator) {
+	case Q931_IE_CN_SI_USER_PROVIDED_NOT_SCREENED:
+		return "User provided, not screened";
+	case Q931_IE_CN_SI_USER_PROVIDED_VERIFIED_AND_PASSED:
+		return "User provided, verified and passed";
+	case Q931_IE_CN_SI_USER_PROVIDED_VERIFIED_AND_FAILED:
+		return "User provided, verified and failed";
+	case Q931_IE_CN_SI_NETWORK_PROVIDED:
+		return "Network provided";
+	default:
+		return "*INVALID*";
+	}
+}
+
+void q931_ie_connected_number_dump(
+	const struct q931_ie *generic_ie,
+	void (*report)(int level, const char *format, ...),
+	const char *prefix)
+{
+	struct q931_ie_connected_number *ie =
+		container_of(generic_ie, struct q931_ie_connected_number, ie);
+
+	report(LOG_DEBUG, "%sType of number = %s (%d)\n", prefix,
+		q931_ie_connected_number_type_of_number_to_text(
+			ie->type_of_number),
+		ie->type_of_number);
+
+	report(LOG_DEBUG, "%sNumbering plan = %s (%d)\n", prefix,
+		q931_ie_connected_number_numbering_plan_identificator_to_text(
+			ie->numbering_plan_identificator),
+		ie->numbering_plan_identificator);
+
+	report(LOG_DEBUG, "%sPresentation indicator = %s (%d)\n", prefix,
+		q931_ie_connected_number_presentation_indicator_to_text(
+			ie->presentation_indicator),
+		ie->presentation_indicator);
+
+	report(LOG_DEBUG, "%sScreening indicator = %s (%d)\n", prefix,
+		q931_ie_connected_number_screening_indicator_to_text(
+			ie->screening_indicator),
+		ie->screening_indicator);
+
+	report(LOG_DEBUG, "%sNumber = %s\n", prefix,
+		ie->number);
+}
---
+++ libq931/libq931/ie_connected_number.h
@@ -0,0 +1,128 @@
+/*
+ * vISDN DSSS-1/q.931 signalling library
+ *
+ * Copyright (C) 2004-2005 Daniele Orlandi
+ *
+ * Authors: Daniele "Vihai" Orlandi <daniele@orlandi.com>
+ *
+ * This program is free software and may be modified and distributed
+ * under the terms and conditions of the GNU General Public License.
+ *
+ */
+
+#ifndef _LIBQ931_IE_CONNECTED_NUMBER_H
+#define _LIBQ931_IE_CONNECTED_NUMBER_H
+
+#include <libq931/ie.h>
+
+/*********************** Calling Party Number *************************/
+
+enum q931_ie_connected_number_type_of_number
+{
+	Q931_IE_CN_TON_UNKNOWN			= 0x0,
+	Q931_IE_CN_TON_INTERNATIONAL		= 0x1,
+	Q931_IE_CN_TON_NATIONAL			= 0x2,
+	Q931_IE_CN_TON_NETWORK_SPECIFIC		= 0x3,
+	Q931_IE_CN_TON_SUBSCRIBER		= 0x4,
+	Q931_IE_CN_TON_ABBREVIATED		= 0x6,
+	Q931_IE_CN_TON_RESERVED_FOR_EXT		= 0x7
+};
+
+enum q931_ie_connected_number_numbering_plan_identificator
+{
+	Q931_IE_CN_NPI_UNKNOWN			= 0x0,
+	Q931_IE_CN_NPI_ISDN_TELEPHONY		= 0x1,
+	Q931_IE_CN_NPI_DATA			= 0x3,
+	Q931_IE_CN_NPI_TELEX			= 0x4,
+	Q931_IE_CN_NPI_NATIONAL_STANDARD	= 0x8,
+	Q931_IE_CN_NPI_PRIVATE			= 0x9,
+	Q931_IE_CN_NPI_RESERVED_FOR_EXT		= 0xf
+};
+
+enum q931_ie_connected_number_presentation_indicator
+{
+	Q931_IE_CN_PI_PRESENTATION_ALLOWED	= 0x0,
+	Q931_IE_CN_PI_PRESENTATION_RESTRICTED	= 0x1,
+	Q931_IE_CN_PI_NOT_AVAILABLE		= 0x2,
+};
+
+enum q931_ie_connected_number_screening_indicator
+{
+	Q931_IE_CN_SI_USER_PROVIDED_NOT_SCREENED	= 0x0,
+	Q931_IE_CN_SI_USER_PROVIDED_VERIFIED_AND_PASSED	= 0x1,
+	Q931_IE_CN_SI_USER_PROVIDED_VERIFIED_AND_FAILED	= 0x2,
+	Q931_IE_CN_SI_NETWORK_PROVIDED			= 0x3,
+};
+
+struct q931_ie_connected_number
+{
+	struct q931_ie ie;
+
+	enum q931_ie_connected_number_type_of_number
+		type_of_number;
+	enum q931_ie_connected_number_numbering_plan_identificator
+		numbering_plan_identificator;
+
+	enum q931_ie_connected_number_presentation_indicator
+		presentation_indicator;
+	enum q931_ie_connected_number_screening_indicator
+		screening_indicator;
+
+
+	char number[21];
+};
+
+struct q931_ie_connected_number *q931_ie_connected_number_alloc(void);
+struct q931_ie *q931_ie_connected_number_alloc_abstract(void);
+
+#ifdef Q931_PRIVATE
+
+struct q931_ie_connected_number_onwire_3
+{
+#if __BYTE_ORDER == __BIG_ENDIAN
+	__u8 ext:1;
+	__u8 type_of_number:3;
+	__u8 numbering_plan_identificator:4;
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
+	__u8 numbering_plan_identificator:4;
+	__u8 type_of_number:3;
+	__u8 ext:1;
+#endif
+} __attribute__ ((__packed__));
+
+struct q931_ie_connected_number_onwire_3a
+{
+#if __BYTE_ORDER == __BIG_ENDIAN
+	__u8 ext:1;
+	__u8 presentation_indicator:2;
+	__u8 :3;
+	__u8 screening_indicator:2;
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
+	__u8 screening_indicator:2;
+	__u8 :3;
+	__u8 presentation_indicator:2;
+	__u8 ext:1;
+#endif
+} __attribute__ ((__packed__));
+
+void q931_ie_connected_number_register(
+	const struct q931_ie_type *type);
+
+int q931_ie_connected_number_read_from_buf(
+	struct q931_ie *ie,
+	const struct q931_message *msg,
+	int pos,
+	int len);
+
+int q931_ie_connected_number_write_to_buf(
+	const struct q931_ie *generic_ie,
+	void *buf,
+	int max_size);
+
+void q931_ie_connected_number_dump(
+	const struct q931_ie *ie,
+	void (*report)(int level, const char *format, ...),
+	const char *prefix);
+
+#endif
+#endif
